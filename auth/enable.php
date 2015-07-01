d23befe38de66603ea6ed821afdd44cdaee3614f
Dennis Calvert <Dennis@denniscalvert.net>
2015-06-30 20:58:24 -0500

    Removed auth directory

--- auth/enable.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * If you are running this along side a wordpress site,
- * you can uncomment out the below line and use WP for user authenitcation
- */
-
-//require_once('wp-basic.php');
-?>
